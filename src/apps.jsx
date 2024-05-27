73bd1ca53e20775d42773b078001ea7f37d26d7d
GRRINO <yy79250@gmail.com>
2024-05-27 18:46:38 +0630

    specified todo

--- src/apps.jsx
+++ src/apps.jsx
@@ -2,41 +2,40 @@ import { useEffect, useReducer, useState } from "react";
 
 const App = () => {
 
-  const [todos,setTodos] = useState([]);
+  const [id,setId] = useState([]);
+  const [error,setError] =useState(false);
+  const [todo,setTodo] = useState(undefined);
 
-  useEffect((_) => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
-    const respond = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const getData = async (e) =>{
+    e.preventDefault();
+    if(id < 1){
+      setError(true);
+      setId("");
+      return
+    }
+    const respond = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
     const data = await respond.json();
-    setTodos(data)
-    
-  };
-
+    setError(false)
+    setTodo(data)
+    setId("");
+  }
   return(
     <section>
-      <table>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>title</th>
-          <th>completed</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          todos.map(todo=>(
-            <tr key={todo.id}>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              <td>{todo.completed ? <p className="done">Done</p> : <p className="none">None</p>}</td>
-            </tr>
-            
-          ))
-        }
-      </tbody>
-    </table>
+      <form onSubmit={getData}>
+      <input type="number" value={id} onChange={(e) => {setId(e.target.value)} } />
+      <button type="submit">Get Data</button>
+      </form>
+      <div>
+      
+        {error && <h1>Please Enter a invalid id.(example 1,2,3,etc ...) </h1>}
+        {todo && 
+        (<div>
+          <h1>id - {todo.id}</h1>
+          <h1>title - {todo.title}</h1>
+          <h1>userId - {todo.userId}</h1>
+          <h1>completed - {todo.completed ? <span>completed</span> : <span>notCompleted</span>}</h1>
+        </div>)}
+      </div>
     </section>
   )
 
